936e7199eebf47dead595605b7c6f6b14fa03624
luiscostalafc <luiscostalafc@gmail.com>
2020-10-06 15:55:06 -0300

    docs: update README

--- app/Validators/PhoneSchema.ts
+++ app/Validators/PhoneSchema.ts
@@ -2,9 +2,9 @@ import { schema } from '@ioc:Adonis/Core/Validator'
 
 export const PhoneSchema = schema.create({
   type: schema.string(),
-  area_code: schema.string(),
+  //area_code: schema.string(),
   phone: schema.string(),
   whatsapp: schema.boolean(),
-  inactive: schema.boolean(),
+  inactive: schema.boolean.optional(),
   obs: schema.string.optional(),
 })
