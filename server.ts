c2c5f45d116f33fb63b528d95b0f8228f23bb25a
EmersonBraun <emersonfbraun@gmail.com>
2021-03-31 20:47:27 -0300

    fix: some imports

--- server.ts
+++ server.ts
@@ -10,10 +10,11 @@
 |
 */
 
+import { Ignitor } from '@adonisjs/core/build/standalone'
 import 'reflect-metadata'
 import sourceMapSupport from 'source-map-support'
-import { Ignitor } from '@adonisjs/core/build/standalone'
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
 new Ignitor(__dirname).httpServer().start()
+console.log('🚀  Servidor rodando bicho!!! 🤩')
