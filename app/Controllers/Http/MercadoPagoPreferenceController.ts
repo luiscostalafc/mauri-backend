5176e5404ebccffd1bf69834927f6deef40535d1
Lucas Becker <lucasgabrielbec@gmail.com>
2022-02-24 23:02:05 -0300

    feat: done with sending email on order

--- app/Controllers/Http/MercadoPagoPreferenceController.ts
+++ app/Controllers/Http/MercadoPagoPreferenceController.ts
@@ -7,12 +7,10 @@ import { generateRandomId } from 'App/utils/createRandomId'
 import OrdersRepository from 'App/Repositories/OrdersRepository'
 import OrderDetail from 'App/Models/OrderDetail'
 import OrderHasProduct from 'App/Models/OrderHasProduct'
-
+import Mail from '@ioc:Adonis/Addons/Mail'
 
 export default class MercadopagoPreferenceController {
-  constructor(
-    private readonly preferenceService = MercadoPagoPreferencesService
-  ) { }
+  constructor(private readonly preferenceService = MercadoPagoPreferencesService) {}
 
   async createPreference({ request, response }: HttpContextContract) {
     const { totalAmount, items, user } = request.all()
@@ -21,22 +19,22 @@ export default class MercadopagoPreferenceController {
 
     const preferenceData = {
       items: items,
-      external_reference: preferenceReference
+      external_reference: preferenceReference,
     }
 
     const orderData = {
       order_status_id: 1,
       provider_id: 1,
       delivery_id: 1,
-      user_id: parsedUser.id
+      user_id: parsedUser.id,
     }
     const createdOrder = await OrdersRepository.create(orderData)
 
-    const orderHasProductsData = items.map(product => ({
+    const orderHasProductsData = items.map((product) => ({
       product_id: product.product_id,
       quantity: product.quantity,
       amount: product.unit_price * product.quantity,
-      order_id: createdOrder.data.id
+      order_id: createdOrder.data.id,
     }))
 
     await OrderHasProduct.createMany(orderHasProductsData)
@@ -48,21 +46,22 @@ export default class MercadopagoPreferenceController {
       extra_amount: totalAmount,
       intallment_quantity: 1,
       intallment_value: totalAmount,
-      order_id: createdOrder.data.id
+      order_id: createdOrder.data.id,
     }
-
 
     await OrderDetailsRepository.create(orderDetail)
     const createdPreference = await this.preferenceService.createPreferenceMP(preferenceData, {})
+
+    await this._sendEmailWithOrder({
+      username: parsedUser.name,
+      order_id: createdOrder.data.id,
+      totalAmount,
+    })
     return response.status(201).json(createdPreference)
   }
 
   async registerReturn({ request, response }: HttpContextContract) {
-    const {
-      status,
-      external_reference,
-      payment_type } = request.all()
-
+    const { status, external_reference, payment_type } = request.all()
 
     const orderDetailToUpdate = await OrderDetail.findBy('reference', external_reference)
     await orderDetailToUpdate?.merge({ order_status: status, payment_method: payment_type })
@@ -79,7 +78,18 @@ export default class MercadopagoPreferenceController {
 
   async notifyPayment({ request, response }: HttpContextContract) {
     const data = request.all()
+  }
 
+  private async _sendEmailWithOrder(data) {
+    await Mail.sendLater((message) => {
+      message
+        .from('vendas@liconnection.com')
+        .to('mauri@mauri.com')
+        .subject('Nova compra no liconnection!').html(`
+        <h1>Novo pedido</h1>
+        <span>${data.username} comprou R$ ${data.totalAmount} em produtos</span> <br />
+        <h3>Acesse este link para checar o pedido </h3><a href='${process.env.FRONTEND_URL}/admin/orders/details/${data.order_id}'>link</a>
+      `)
+    })
   }
 }
-
--- app/Repositories/OrderDetailsRepository.ts
+++ app/Repositories/OrderDetailsRepository.ts
@@ -3,7 +3,15 @@ import Logger from '@ioc:Adonis/Core/Logger'
 import OrderDetail from 'App/Models/OrderDetail'
 import { mountResponse } from 'App/Services/ResponseUtils'
 import Database from '@ioc:Adonis/Lucid/Database'
-import { all, create, createOrUpdate, find, findAndDelete, findAndUpdate, first } from '../Services/CRUD'
+import {
+  all,
+  create,
+  createOrUpdate,
+  find,
+  findAndDelete,
+  findAndUpdate,
+  first,
+} from '../Services/CRUD'
 
 class OrderDatailsRepository {
   protected model: any
@@ -13,32 +21,32 @@ class OrderDatailsRepository {
   protected statusCode = 400
   protected options = 0
 
-  public logError (func, error) {
+  public logError(func, error) {
     Logger.warn(`Repository ${func} Error: ${error}`)
   }
 
-  constructor () {
+  constructor() {
     this.model = OrderDetail
   }
 
-  async first () {
+  async first() {
     return await first(this.model)
   }
 
-  async all () {
+  async all() {
     return await all(this.model)
   }
 
-  async find (id) {
+  async find(id) {
     return await find(this.model, id)
   }
 
-  async search (query) {
+  async search(query) {
     let contentError = ''
     let data: any
-    try{
+    try {
       data = await this.model.query().where(query)
-    } catch(error) {
+    } catch (error) {
       console.log(error)
       contentError = error
     }
@@ -46,31 +54,32 @@ class OrderDatailsRepository {
     return mountResponse(data, contentError, 'load')
   }
 
-  async create (data: any) {
+  async create(data: any) {
     return await create(this.model, data)
   }
 
-  async createOrUpdate (register: any, data: any) {
+  async createOrUpdate(register: any, data: any) {
     return await createOrUpdate(this.model, register, data)
   }
 
-  async findAndUpdate (id: any, data: any) {
+  async findAndUpdate(id: any, data: any) {
     return await findAndUpdate(this.model, id, data)
   }
 
-  async findAndDelete (id: any) {
+  async findAndDelete(id: any) {
     return await findAndDelete(this.model, id)
   }
 
-  async getOrdersWithProducts(id: number): Promise<any>{
-    const result =  await Database.rawQuery(`select * from order_details d
+  async getOrdersWithProducts(id: number): Promise<any> {
+    const result =
+      await Database.rawQuery(`select d.*, o.*, u.*, ohp.*, p.*, p.name as product_name, ohp.quantity as quantity_bought from order_details d
     left join orders o on d.order_id = o.id
     left join users u on u.id = o.user_id
     left join order_has_products ohp on ohp.order_id = o.id
     left join products p on ohp.product_id = p.id where o.id = '${id}'`)
 
     return result.rows
-}
+  }
 }
 
 export default new OrderDatailsRepository()
